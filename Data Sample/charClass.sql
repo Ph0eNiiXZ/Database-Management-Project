c41f590318cfa179e3879f6519c432223c5f17c9
Jirapit Sr <91188315+Ph0eNiiXZ@users.noreply.github.com>
2022-10-13 19:08:52 +0700

    Add files via upload

---
+++ Data Sample/charClass.sql
@@ -0,0 +1,7 @@
+INSERT INTO `charClass` (`charClassID`,`charClassName`,`charClassDesc`)
+VALUES
+  (1,"Swordsman","All About Aggression"),
+  (2,"Engineer","Everyone Gets Attacked"),
+  (3,"Rogue","Get Everything Away"),
+  (4,"Witch","Rule The Distance"),
+  (5,"Destroyer","Security Through Offense");
---
+++ Data Sample/character.sql
@@ -0,0 +1,36 @@
+INSERT INTO `character` (`charID`,`charName`,`charDescription`,`charClassID`,`charHP`,`charATK`,`charDEF`,`price`)
+VALUES
+  (1,"Honorato","Envy,",4,563,748,332,5000),
+  (2,"Robert","Greed,",1,950,73,203,7500),
+  (3,"Kennan","Envy,",3,29,348,838,10000),
+  (4,"Ruby","Envy,",1,11,259,881,12500),
+  (5,"Lillith","Pride,",3,528,395,462,15000),
+  (6,"Nola","Gluttony,",1,730,159,563,17500),
+  (7,"Cecilia","Gluttony,",4,248,615,422,20000),
+  (8,"Fuller","Gluttony,",4,99,417,586,22500),
+  (9,"Lawrence","Pride,",4,873,925,970,25000),
+  (10,"Buckminster","Greed,",1,117,497,577,27500);
+INSERT INTO `character` (`charID`,`charName`,`charDescription`,`charClassID`,`charHP`,`charATK`,`charDEF`,`price`)
+VALUES
+  (11,"Eliana","Pride,",4,113,371,352,30000),
+  (12,"Maisie","Wrath,",3,365,317,435,32500),
+  (13,"Petra","Gluttony,",4,350,628,180,35000),
+  (14,"Clayton","Greed,",5,949,442,774,37500),
+  (15,"Bruce","Pride,",5,791,559,810,40000),
+  (16,"Magee","Gluttony,",5,501,489,94,42500),
+  (17,"Paul","Gluttony,",2,324,755,70,45000),
+  (18,"Brielle","Envy,",2,347,938,835,47500),
+  (19,"Todd","Wrath,",2,771,632,663,50000),
+  (20,"Cassady","Gluttony,",5,272,154,5,52500);
+INSERT INTO `character` (`charID`,`charName`,`charDescription`,`charClassID`,`charHP`,`charATK`,`charDEF`,`price`)
+VALUES
+  (21,"Brandon","Wrath,",2,232,612,238,55000),
+  (22,"Benedict","Gluttony,",4,543,319,818,57500),
+  (23,"Rachel","Wrath,",5,65,413,53,60000),
+  (24,"Zenia","Pride,",5,750,834,261,62500),
+  (25,"Troy","Wrath,",2,79,183,493,65000),
+  (26,"Kalia","Lust,",2,113,71,950,67500),
+  (27,"Wyatt","Gluttony,",3,603,554,838,70000),
+  (28,"Kerry","Envy,",5,240,816,36,72500),
+  (29,"Moses","Envy,",2,161,873,74,75000),
+  (30,"Juliet","Wrath,",4,55,193,942,77500);
