67aa7e4252e6878c15102f985abec5e19217203e
Jirapit Sr <91188315+Ph0eNiiXZ@users.noreply.github.com>
2022-10-19 10:54:40 +0700

    Add files via upload

---
+++ Data Sample/monster.sql
@@ -0,0 +1,15 @@
+INSERT INTO `monster` (monsterID, monsterName, monsterDesc, monsterHP, monsterATK, monsterDEF, monsterEXPReward)
+VALUES
+  (1, 'Dreamscream', 'This is a description', 50000, 200, 200, 500),
+  (2, 'Shadowscreamer', 'This is a description', 85263, 652, 111, 9941),
+  (3, 'Blazetree', 'This is a description', 665462, 1234, 500, 43451),
+  (4, 'Vaporfiend', 'This is a description', 20000, 20, 400, 1000),
+  (5, 'The Angry Ooze', 'This is a description', 30201, 99, 99, 420),
+  (6, 'The Noxious Abortion', 'This is a description', 69696, 420, 200, 500),
+  (7, 'The Hidden Shrieker', 'This is a description', 11111, 111, 111, 1111),
+  (8, 'The Hidden Razorback Rhino', 'This is a description', 234567, 856, 951, 6666),
+  (9, 'Blue Zenith', 'This is a description', 72727, 727, 727, 7272),
+  (10, 'Lucky', 'This is a description', 777777, 777, 777, 77777)
+  ;
+  
+  
