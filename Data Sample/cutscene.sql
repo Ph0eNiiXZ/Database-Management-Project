3fd9e07b823ed0ada9f072c0c63c4c20b36abf8d
Jirapit Sr <91188315+Ph0eNiiXZ@users.noreply.github.com>
2022-10-19 11:03:16 +0700

    Add files via upload

---
+++ Data Sample/cutscene.sql
@@ -0,0 +1,9 @@
+INSERT INTO `lastfantasy`.`cutscene`
+(`cutsceneID`,
+`cutsceneName`)
+VALUES
+(1, 'Hi, welcome to osu!'),
+(2, 'When you see it! When you really seee it!!'),
+(3, 'The One Piece...The One Piece is real~~'),
+(4, 'Kono Dio Da'),
+(5, "I am the storm that is approaching...");
