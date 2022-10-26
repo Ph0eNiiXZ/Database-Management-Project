939be019ca7c794c537d2a1a8c71800262462708
Jirapit Sr <91188315+Ph0eNiiXZ@users.noreply.github.com>
2022-10-26 18:37:40 +0700

    Add files via upload

---
+++ Data Sample/character_dialog.sql
@@ -0,0 +1,12 @@
+INSERT INTO `last_fantasy`.`character_dialog`
+(`dialogID`,
+`cutsceneID`,
+`characterID`,
+`order`,
+`dialog`)
+VALUES
+(1,1,2,1,"hi this is a dialog"),
+(2,2,4,2,"hi this is another dialog"),
+(3,3,6,3,"hi this is another another dialog"),
+(4,4,8,4,"hi this is another another another dialog"),
+(5,5,10,5,"hi this is another another another another dialog");
