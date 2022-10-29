55c7da7a616f503e8ae56f046be44800b1f1ac8c
Jirapit Sr <91188315+Ph0eNiiXZ@users.noreply.github.com>
2022-10-29 18:57:43 +0700

    Add files via upload

---
+++ Data Sample/dungeon_quest_prereqs.sql
@@ -0,0 +1,9 @@
+INSERT INTO `last_fantasy`.`dungeon_quest_prereqs`
+(`dungeonID`,
+`questPreReqsID`)
+VALUES
+(1,1),
+(2,2),
+(3,3),
+(4,4),
+(5,5);
---
+++ Data Sample/quest.sql
@@ -0,0 +1,14 @@
+INSERT INTO `last_fantasy`.`quest`
+(`questID`,
+`questName`,
+`questDescription`,
+`prereqsEXP`,
+`expReward`,
+`moneyReward`)
+VALUES
+(1, "Quest of a new beginning", "The first quest ever", 0, 100, 1000),
+(2, "The rampaging slime", "Oh no what is that slime doing?!?!?!", 100, 100, 150),
+(3, "Let's go slay some goblins", "By the name of the goblin slayer, all goblins shall be killed by my hands", 200, 200, 200),
+(4, "Emergency at the town of beginnings", "####ERROR####...?", 400, 500, 1000),
+(5, "A prelude to the end of time, the start of an unstoppable apocalyse", "FINORZA", 900, 666, 666),
+(999, "FINORZA", "THE END", 99999, 99999, 99999);
