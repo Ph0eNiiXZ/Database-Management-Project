66a604fc729d0d4663f621b2918d80f48dd0e9be
Jirapit Sr <91188315+Ph0eNiiXZ@users.noreply.github.com>
2022-10-30 13:37:58 +0700

    Add files via upload

---
+++ Data Sample/monster_dialog.sql
@@ -0,0 +1,13 @@
+INSERT INTO `last_fantasy`.`monster_dialog`
+(`dialogID`,
+`cutsceneID`,
+`monsterID`,
+`order`,
+`dialog`)
+VALUES
+(1,1,1,1,"OSU!"),
+(2,1,1,2, "I love smashing keyboards?"),
+(3,2,9,1,"When you see it!"),
+(4,2,9,2,"When you freaking see it AHHHH!!!!"),
+(5,5,10,1,"I am now motivated"),
+(6,5,10,2,"Time to confess to p...");
